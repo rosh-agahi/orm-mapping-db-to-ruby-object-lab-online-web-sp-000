52d07ff0bed2e91d9390b6aa3e21a8d018fc3552
Roshanak Agahi <rosh.agahi@gmail.com>
2019-10-09 17:11:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,3 +65,15 @@ class Student
     DB[:conn].execute(sql)
   end
 end
+
+def self.all_students_in_grade_9
+    
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 9
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
