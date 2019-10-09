c32407c2a01266eb4d05ce6ce4cb805bfefcefd6
Roshanak Agahi <rosh.agahi@gmail.com>
2019-10-09 17:01:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,8 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
-    
+    new_student = student.new
+    new_student = row[0]
   end
 
   def self.all
