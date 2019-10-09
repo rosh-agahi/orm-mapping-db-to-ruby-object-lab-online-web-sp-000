9819f46a8c357f9e2185bfe8eef0e6734ac76e7e
Roshanak Agahi <rosh.agahi@gmail.com>
2019-10-09 17:19:55 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -77,10 +77,21 @@ class Student
       end
   end
     
-  def self.all_students_in_grade_9
+  def self.students_below_12th_grade
     sql = <<-SQL
       SELECT * FROM students
-      WHERE grade = 9
+      WHERE grade < 12
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+      end
+  end
+  
+  def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade < 12
     SQL
 
     DB[:conn].execute(sql).map do |row|
