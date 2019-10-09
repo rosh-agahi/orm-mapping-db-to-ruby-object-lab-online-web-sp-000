ffe1145d8c6bbf4bcae3a1b0c19c3e668c33ec3f
Roshanak Agahi <rosh.agahi@gmail.com>
2019-10-09 17:26:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -88,6 +88,18 @@ class Student
       end
   end
   
+  def self.first_X_students_in_grade_10(X)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 10 
+      limit ?
+    SQL
+
+    DB[:conn].execute(sql,X).map do |row|
+      self.new_from_db(row)
+      end
+  end
+
 
     
 end
